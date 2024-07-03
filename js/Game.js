275d301fa59cdfa81f9c72a17f2a38aae303222e
mikeovas <mikeovas@gmail.com>
2024-07-03 18:37:04 -0400

    game & phrase class started

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,12 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+
+class Game {
+    constructor() {
+      this.missed = 0;          // Tracks the number of missed guesses
+      this.phrases = [];        // An array to hold Phrase objects
+      this.activePhrase = null; // The currently active Phrase object
+    }
+  }
