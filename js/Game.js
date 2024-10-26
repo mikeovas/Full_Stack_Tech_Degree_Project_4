3369010d27ceeb524b569a5e464222e8f34b7e47
mikeovas <mikeovas@gmail.com>
2024-10-26 18:07:31 -0400

    removeLIfe() & gameOver() functions working

--- js/Game.js
+++ js/Game.js
@@ -5,6 +5,8 @@
 
 class Game {
     constructor() {
+      const overlay = document.querySelector('#overlay');
+
       this.missed = 0;                                                                // Tracks the number of missed guesses
       this.phrases = [
         new Phrase('May the Force be with you'),
@@ -27,19 +29,75 @@ class Game {
     };
 
     startGame() {
-      const screenOverlay = document.querySelector('#overlay');
-
-      screenOverlay.style.display = 'none';
-      this.activePhrase = this.getRandomPhrase();
+      overlay.style.display = 'none';
+      this.activePhrase = this.getRandomPhrase();                                   
       this.activePhrase.addPhraseToDisplay();
 
     };
 
-    handleInteraction() {
 
-      // phrase.checkLetter(); 
-      // phrase.showMatchedLetter();
+    
+    /**
+    * Checks for all letters in the phrase to have been revealed
+    * @return {boolean} True if game has been won, false if game wasn't won
+    **/
+      checkForWin() {
+        const lettersInPhrase = document.getElementsByClassName('letter');
+        const shownLetters = document.getElementsByClassName('show');
+        return lettersInPhrase.length === shownLetters.length;
     };
+
+    /**
+    * Increases the value of the missed property
+    * Removes a life from the scoreboard
+    * Checks if player has remaining lives and ends game if player is out
+    */
+      removeLife() {
+        const hearts = document.querySelectorAll('.tries img');
+        hearts[this.missed].src = 'images/lostHeart.png';                      // replace live heart with a lost heart
+        this.missed += 1;
+        console.log(hearts);
+
+        if (this.missed === 5) {
+          this.gameOver(false);                                             // End the game if all lives are lost
+      }
+
+      };
+
+    /**
+    * Ends the game and displays a win or loss message
+    * @param {boolean} gameWon - Whether or not the user won the game
+    */
+      gameOver(gameWon) {
+        const message = document.getElementById('game-over-message');
+
+        overlay.style.display = 'flex';                                    // Show the overlay again
+
+        if (gameWon) {
+            overlay.className = 'win';                                     // Set class to 'win'
+            message.textContent = 'Congratulations, you won!';
+        } else {
+            overlay.className = 'lose';                                    // Set class to 'lose'
+            message.textContent = 'Sorry, better luck next time!';
+        }
+
+        // Reset missed guesses
+          this.missed = 0;
+
+        // Reset hearts
+          const hearts = document.querySelectorAll('.tries img');
+          hearts.forEach(heart => heart.src = 'images/liveHeart.png');
+    };
+
+
+
+
+
+// The logic and branching to handle several steps to run this application are included in this method
+handleInteraction() {
+  // phrase.checkLetter(); 
+  // phrase.showMatchedLetter();
+};
 
 
 
